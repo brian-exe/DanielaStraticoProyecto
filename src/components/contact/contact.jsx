4473431a990dae8ade88ffe3ea679bf6a9328479
Ezequiel Aciar <ez_aciar@farmacity.com.ar>
2022-04-20 01:16:44 -0300

    Se incorpora envio mensaje contacto via whatsapp

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,10 +1,34 @@
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
 import React from 'react';
+const {useState} = React;
 
 function Contact() {
-    function handleClick() {
-        console.log('hola')
+    const [nombre, setNombre] = useState('');
+    const [mensaje, setMensaje] = useState('');
+    //const nroTelefono = "541168090888";
+    const nroTelefono = "541111111111";
+
+
+    const onNombreChange = ({target:{value}}) => setNombre(value);
+    const onMensajeChange = ({target:{value}}) => setMensaje(value);
+
+    const openInNewTab = (url) => {
+        const newWindow = window.open(url, '_blank', 'noopener,noreferrer')
+        if (newWindow) newWindow.opener = null
     }
+
+    const buildWhatsappUrl= (nombre, mensaje)=> {
+        return "https://wa.me/"+ nroTelefono + "?text=Hola mi nombre es *" + nombre.trim() + "*. Mi consulta es: *" + mensaje.trim() + "*."
+    }
+
+    //https://mail.google.com/mail/u/0/?source=mailto&to=daniela.stratico@gmail.com&fs=1&tf=cm&body=hola&su=consulta
+
+    const onSubmit = e => {
+        e.preventDefault()
+        const whatsappUrl = buildWhatsappUrl(nombre, mensaje);
+        openInNewTab(whatsappUrl);
+    }
+
     return (
         <Container className="container-contact" >
             <Row>
@@ -19,17 +43,23 @@ function Contact() {
                     <Row><h4 className="text-presentacion">Abrazo, <br></br> Daniela.</h4></Row>
                 </Col>
                 <Col>
-                    <Form className="col-md-6">
-                        <Form.Group className="mb-3" controlId="nombre">
-                            <Form.Control type="text" placeholder="Nombre" />
-                        </Form.Group>
-                        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-                            <Form.Control style={{ resize: 'none' }} as="textarea" rows={3} placeholder="Mensaje..." />
-                        </Form.Group>
-                        <Button onClick={handleClick} className="btn-pink">
-                            Submit
-                        </Button>
-                    </Form>
+                    <Row>
+                        <Form className="col-md-6">
+                            <Form.Group className="mb-3" controlId="nombre">
+                                <Form.Control type="text" placeholder="Nombre" 
+                                        onChange={onNombreChange} 
+                                        value={nombre} />
+                            </Form.Group>
+                            <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                                <Form.Control style={{ resize: 'none' }} as="textarea" rows={3} placeholder="Mensaje..."
+                                        onChange={onMensajeChange} 
+                                        value={mensaje} />
+                            </Form.Group>
+                            <Button type="submit" onClick={onSubmit} className="btn-pink">
+                                Enviar
+                            </Button>
+                        </Form>
+                    </Row>
                 </Col>
             </Row>
         </Container>
