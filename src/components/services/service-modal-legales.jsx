3a1876ec306cb84dcacc7d341ede4fc8a3d6b53f
Camila Paz <cambpaz@gmail.com>
2022-04-21 15:32:31 -0300

    se agrego diseño de modales

---
+++ src/components/services/service-modal-legales.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const ModalLegales = () => {
+  return (
+    <>
+    <h1>Comunicacion</h1>
+    </>
+  )
+}
--- src/components/services/services.jsx
+++ src/components/services/services.jsx
@@ -8,49 +8,110 @@ import Modal from 'react-bootstrap/Modal'
 import { Button } from 'react-bootstrap'
 
 function Services() {
-        const [show, setShow] = useState(false);
-        const handleClose = () => setShow(false);
-        const handleShow = () => setShow(true);
-        return (
-            <Container className="container-services">
-                <Row>
-                    <div className='container-title-exp'>
-                        <h2 className='title-exp'>Servicios</h2>
-                    </div>
-                </Row>
-                <Row className="services-cards-container" >
-                    {/* <div onClick={handleShow}> */}
-                    <ServiceCard 
-                        title="Legales"
-                        imgSrc={legalesImg} />
-                    {/* </div> */}
-                    <ServiceCard
-                        title="Comunicación"
-                        imgSrc={comunicacionImg} />
-                    <ServiceCard
-                        title="Consultoría"
-                        imgSrc={coachingImg} />
-                </Row>
-                {/* <Button variant="primary" onClick={handleShow}>
-                    Launch demo modal
-                </Button> */}
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    return (
+        <Container className="container-services">
+            <Row>
+                <div className='container-title-exp'>
+                    <h2 className='title-exp'>Servicios</h2>
+                </div>
+            </Row>
+            <Row className="services-cards-container" >
+                {/* <div onClick={handleShow}> */}
+                <ServiceCard
+                    title="Legales"
+                    imgSrc={legalesImg} />
+                {/* </div> */}
+                <ServiceCard
+                    title="Comunicación"
+                    imgSrc={comunicacionImg} />
+                <ServiceCard
+                    title="Consultoría"
+                    imgSrc={coachingImg} />
+            </Row>
+            <Button variant="primary" onClick={handleShow}>
+                Launch demo modal
+            </Button>
 
-                <Modal show={show} onHide={handleClose}>
-                    <Modal.Header closeButton>
-                        <Modal.Title>Modal heading</Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-                    <Modal.Footer>
-                        <Button variant="secondary" onClick={handleClose}>
-                            Close
-                        </Button>
-                        <Button variant="primary" onClick={handleClose}>
-                            Save Changes
-                        </Button>
-                    </Modal.Footer>
-                </Modal>
-            </Container>
-        );
-    }
 
-    export default Services;
+
+            {/*  DEJO ABAJO LOS TRES MODALES PARA ASOCIAR AL SERVICE CARD */}
+            
+            <Modal show={show} onHide={handleClose} centered>
+                <Modal.Header closeButton>
+                    <Modal.Title>Comunicación</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <p>
+                        <b>¿Para qué vas a esperar que el conflicto escale si lo podemos trabajar antes?</b><br></br>
+                        Evitemos que tus conflictos se transformen en un problema legal, un conflicto laboral y/o que impacten en tus finanzas.
+                        El cambio es constante, los conflictos también lo son y hoy en día es saludable incorporar nuevas herramientas para gestionar las necesidades del negocio y del equipo colaborador.
+                        <br></br>
+                        <br></br>
+                        <b>¿Qué herramientas ofrezco para liderar el cambio?</b> <br></br>
+                        El modelo de la programación neurolingüística, la ontología del lenguaje y el pensamiento sistémico.
+                        <br></br>
+                        <br></br>
+                        <b>¿Cómo te acompaño a incorporar herramientas de estos modelos?</b><br></br>
+                        Entrevistas para obtener información de las áreas o personas afectadas en la empresa.
+                        Diagnóstico de la problemática del negocio.
+                        Diseñaremos las facilitaciones o capacitaciones, individuales o grupales, que se ajusten a tu necesidad:
+                        -Habilidades blandas: escucha, empatía.
+                        -Organización: gestión del tiempo, educación emocional. Desarrollo de pensamiento sistémico.
+
+                    </p>
+                </Modal.Body>
+            </Modal>
+
+            <Modal show={show} onHide={handleClose} centered>
+                <Modal.Header closeButton>
+                    <Modal.Title>Legales</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <p>
+                        Asesoramiento generalista del negocio. <br></br><br></br>
+
+                        Representación Prejudicial – Negociaciones. <br></br><br></br>
+
+                        Contratos: Redacción, administración, renegociación y terminación de contratos. (comerciales, civiles, laborales). <br></br><br></br>
+
+                        Societario: Redacción de actas obligatorias. Inscripción de trámites. <br></br><br></br>
+
+                        Administración de personal: Reclamos laborales y sindicales. <br></br><br></br>
+
+                        Representación en litigios.
+
+                    </p>
+                </Modal.Body>
+            </Modal>
+
+            <Modal show={show} onHide={handleClose} centered>
+                <Modal.Header closeButton>
+                    <Modal.Title>Consultoría</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <p>
+                        Análisis integral de las problemáticas de la empresa y abordaje de gestiones humanas y/o técnicas para arrivar a una solución:<br></br><br></br>
+
+                        Entrevistas para obtener información de las áreas o personas afectadas en la empresa.<br></br><br></br>
+
+                        Diagnóstico de la problemática del negocio con los indicadores recabados.<br></br><br></br>
+
+                        Presentación de la estrategia o plan de acción.<br></br><br></br>
+
+                        Implementación de la estrategia, ajustes y correcciones necesarias.<br></br><br></br></p>
+
+                    <p>Acompañamiento a la empresa en la evolución de su plan de acción:</p>
+                    <ul>
+                        <li>Capacitaciones.</li>
+                        <li>Facilitaciones.</li>
+                    </ul>
+                </Modal.Body>
+            </Modal>
+        </Container>
+    );
+}
+
+export default Services;
