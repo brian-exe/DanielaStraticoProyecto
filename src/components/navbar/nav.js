c9cdc9ef3be410f96fb040874b85820c981e790f
Camila Paz <cambpaz@gmail.com>
2022-05-09 20:17:01 -0300

    .

--- src/components/navbar/nav.js
+++ src/components/navbar/nav.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import { Nav, Container, Navbar} from 'react-bootstrap';
-
+import imgLogo from '../../assets/logo1.png'
 
 export const NavMenu = () => {
     return (
         <Navbar expand="md">
             <Container>
-                <Navbar.Brand href="#home">Daniela Stratico</Navbar.Brand>
+                <Navbar.Brand href="#home"><img className='imgLogo' src={imgLogo} alt="" /></Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#experience">Sobre Mi</Nav.Link>
-                        <Nav.Link href="#services">Servicios</Nav.Link>
-                        <Nav.Link href="#contact">Contacto</Nav.Link>
+                        <Nav.Link href="#experience">SOBRE MI</Nav.Link>
+                        <Nav.Link href="#services">SERVICIOS</Nav.Link>
+                        <Nav.Link href="#contact">CONTACTO</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
