30fd5c5393aec5b05941e605a0d3a7df18569a37
Ezequiel Aciar <ez_aciar@farmacity.com.ar>
2022-05-12 20:04:52 -0300

    Se incorpora logo para instagram (incorrecto)

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -2,7 +2,7 @@ import { Col } from "react-bootstrap";
 import React from 'react';
 import logoWpp from '../../assets/logoWhatsapp.png'
 import logoLinkedIn from '../../assets/logoLinkedin.png'
-
+import logoInstagram from '../../assets/logoInstagram.svg'
 
 function Footer() {
     return (
@@ -13,6 +13,7 @@ function Footer() {
                 <div>
                     <a href="https://wa.me/541168090888"><img className="logoFooter" src={logoWpp} alt="logo whatsapp" /></a>
                     <a href="https://www.linkedin.com/in/daniela-stratico/" ><img className="logoFooter" src={logoLinkedIn} alt="" /></a>
+                    <a href="https://www.instagram.com/" ><img className="logoFooter" src={logoInstagram} alt="" /></a>
                 </div>
                 <p className="copyright"> Copyright © Daniela Stratico | Todos los derechos reservados.</p>
             </Col>
